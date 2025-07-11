ba0f7e69465a9537a72d9626504f29179c5b2447
Menuka Kasun Chathuranga <119678055+kasundhs@users.noreply.github.com>
2025-07-11 22:07:48 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_w5RancdcO', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'afbr5l7tlll569qa7qhm0fdj4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_Cm9FRxX4F', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1g158tvctrn3vmennrhhqi1t38', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
