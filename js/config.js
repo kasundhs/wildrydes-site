fd2dbf39e858c28f00c4c4844cc9f0acd0a5464c
Menuka Kasun Chathuranga <119678055+kasundhs@users.noreply.github.com>
2025-07-11 20:33:05 +0530

    Update config.js

--- js/config.js
+++ js/config.js
@@ -2,7 +2,7 @@ window._config = {
     cognito: {
         userPoolId: 'us-east-1_w5RancdcO', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '2d98rm7vvv7n8nboa6qh0f1ve1', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
